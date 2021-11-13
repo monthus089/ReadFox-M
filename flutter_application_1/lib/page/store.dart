e8cb40ec95d7f148a53adbba6243cbba5802c3c1
monthus089 <91974247+monthus089@users.noreply.github.com>
2021-11-13 21:24:03 +0700

    Kai book slider

--- flutter_application_1/lib/page/store.dart
+++ flutter_application_1/lib/page/store.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class Store extends StatefulWidget {
@@ -9,9 +10,68 @@ class _StoreState extends State<Store> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(' Store')),
-      body: Center(
-        child: Text('', style: TextStyle(fontSize: 40)),
+      body: Column(
+        children: [
+          Container(
+            height: 350,
+            decoration: BoxDecoration(
+                color: Colors.red[300],
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.elliptical(200, 200),
+                    bottomRight: Radius.elliptical(300, 300))),
+            child: Stack(
+              children: [
+                Container(
+                  height: 250,
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.elliptical(300, 300),
+                          bottomRight: Radius.elliptical(300, 300))),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15.0),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Store',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 22,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      // ^ top bar //
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: SizedBox(
+                          height: 10,
+                        ),
+                      ),
+                      CarouselSlider.builder(
+                          viewportFraction: .5,
+                          enableInfiniteScroll: true,
+                          itemCount: 15,
+                          itemBuilder: (BuildContext context, int itemIndex) =>
+                              Container(
+                                width: 130,
+                                color: Colors.blue,
+                              ))
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
